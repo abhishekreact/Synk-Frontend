3066c44d635bd7ea04fa8da1535ca885b787b12c
abhishekreact <abhishekreact.dev@gmail.com>
2025-02-09 16:47:06 +0530

    initial setup code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,180 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { NAV_LINK_CONFIG } from "./utils/constant";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export default function IndexPage() {
+  return (
+    <div className=" w-full min-h-screen  ">
+      <nav className="w-full h-[10%] py-[1.3vmax] px-[4vmax]   flex items-center justify-between ">
+        <div className="logo-wrapper  flex items-center gap-[0.6vmax]">
+          <img src="/synk-logo.png" alt="synk-logo" className="w-[2vmax]" />
+
+          <h3 className="text-[1.6vmax]">Synk</h3>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <ul className="nav-links flex justify-between items-center gap-[2.4vmax]">
+          {NAV_LINK_CONFIG.map((item) => (
+            <li className="text-[--light-grey] text-[1.1vmax]" key={item?.id}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+
+        <div className="beta-access-btn bg-white text-[#030303] rounded-full text-[1.2vmax] px-[1.2vmax] py-[0.5vmax]">
+          Join beta
+        </div>
+      </nav>
+
+      <section className="py-[2vmax] pl-[4vmax]">
+        <div className="hero-container overflow-hidden   flex justify-between items-center">
+          <div className="w-1/2  ">
+            <small className="bg-[#414141] mb-[0.6vmax] inline-block py-[0.8vh] px-[1.6vh] rounded-lg text-white text-[1vmax]">
+              We've raised $3M series A
+            </small>
+            <div className="hero-text-wrapper leading-none text-[4.8vmax] ">
+              <h2>a home for the</h2>
+              <h2>musical minds</h2>
+            </div>
+
+            <p className="hero-content w-1/2 py-[1.6vmax] leading-snug text-[1.2vmax] text-[#aaaaaa]">
+              A place for musicians to connect, collaborate, discover gigs,
+              share their work, and find opportunities.
+            </p>
+
+            <div className="beta-access-btn w-fit bg-white text-[#030303] rounded-full text-[1.2vmax] px-[1.2vmax] py-[0.5vmax]">
+              Join beta
+            </div>
+          </div>
+          <div className="w-1/2">
+            <img src="/hero-ui.png" className="w-full " alt="hero-ui" />
+          </div>
+        </div>
+      </section>
+
+      <section className="py-[4vmax]  px-[4max]">
+        <div className="w-2/3 py-[8vw]  mx-auto ">
+          <div className="feature-content">
+            <h3 className="text-[3vmax] pb-[1.6vmax] leading-none">
+              For anyone with <br /> music to make
+            </h3>
+            <p className="w-1/2  text-[1.2vmax] leading-snug text-[#aaaaaa]">
+              A place for musicians to connect, collaborate, discover gigs,
+              share their work, and find opportunities.
+            </p>
+          </div>
+
+          <div className="w-full bg-[#1d1d1d] mt-[4vmax] rounded-xl">
+            <div className="px-[2vmax] py-[1.2vmax] ">
+              <h4 className="pb-[0.4vmax] text-[1.6vmax]">Expertise</h4>
+              <p className="w-1/2 text-[1.2vmax] leading-snug  text-[#aaaaaa]">
+                Get unlimited design and development with a <br /> simple
+                monthly subscription.
+              </p>
+            </div>
+            <div className="feature-img-wrapper w-full pt-[2vmax] flex justify-end ">
+              <div className="w-[90%]">
+                <img src="./feature-ui-1x01.png" className="w-full" alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full flex gap-[1vmax] mt-[1vmax] ">
+            <div className=" bg-[#1d1d1d] px-[2vmax] pt-[3vmax] w-1/2  rounded-xl">
+              <p className="text-[1.2vmax] leading-snug  text-[#aaaaaa]">
+                Find information across your entire library. Ask questions, get
+                answers, find files instantly.
+              </p>
+
+              <div className="feature-img-wrapper py-[6vmax] flex justify-center">
+                <div className="w-2/3">
+                  <img src="./feature-ui-1x02.png" className="w-full" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className=" bg-[#1d1d1d] px-[2vmax] pt-[3vmax] w-1/2  rounded-xl"></div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-[4vmax]  px-[4vmax]">
+        <div className="news-wrapper ">
+          <div className="tagline text-[2.4vmax] pb-[2vmax]">News</div>
+
+          <div className="news-wrapper-inner  flex gap-[1vmax] ">
+            <div className="news w-1/3  rounded-xl">
+              <div className="news-img-wrapper w-full  rounded-xl overflow-hidden  ">
+                <img src="./news-1x01.png" className="w-full" alt="" />
+              </div>
+
+              <div className="news-content py-[0.6vmax] leading-snug">
+                <h3 className="text-[1.2vmax]">Synk launches new features</h3>
+                <small className="text-[#979797] text-[0.9vmax]">
+                  News / December 19, 2024
+                </small>
+              </div>
+            </div>
+
+            <div className="news w-1/3  rounded-xl">
+              <div className="news-img-wrapper w-full  rounded-xl overflow-hidden  ">
+                <img src="./news-1x02.png" className="w-full" alt="" />
+              </div>
+
+              <div className="news-content py-[0.6vmax] leading-snug">
+                <h3 className="text-[1.2vmax]">Synk launches new features</h3>
+                <small className="text-[#979797] text-[0.9vmax]">
+                  News / December 19, 2024
+                </small>
+              </div>
+            </div>
+
+            <div className="news w-1/3  rounded-xl">
+              <div className="news-img-wrapper w-full  rounded-xl overflow-hidden  ">
+                <img src="./news-1x03.png" className="w-full" alt="" />
+              </div>
+
+              <div className="news-content py-[0.6vmax] leading-snug">
+                <h3 className="text-[1.2vmax]">Synk launches new features</h3>
+                <small className="text-[#979797] text-[0.9vmax]">
+                  News / December 19, 2024
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-[4vmax]  px-[4vmax]">
+        <footer>
+          <div className="text-center py-[8vmax]">
+            <h2 className="text-[3vmax] leading-snug">
+              Let your music <br />
+              do the magic
+            </h2>
+
+            <div className="py-[1.6vmax]">
+
+
+            <button className="beta-access-btn w-fit  bg-white text-[#030303] rounded-full text-[1.2vmax] px-[1.2vmax] py-[0.5vmax]">
+              Join beta
+            </button>
+            </div>
+          </div>
+
+          <ul className="flex pb-[2vmax] justify-center gap-[4vmax] text-[0.9vmax] text-[#969696]">
+
+            <li>Home</li>
+            <li>About</li>
+            <li>Privacy Policy</li>
+
+
+
+          </ul>
+
+          <div className="text-[#5c5c5c] pb-[4vmax] text-center text-[0.9vmax]">
+          More ways to shop: <span className="underline">Find an Apple Store</span>  or <span className="underline">other retailer</span>  near you. Or call 000800 040 1966.
+          </div>
+        </footer>
+      </section>
     </div>
   );
 }
---
+++ src/app/utils/constant.ts
@@ -0,0 +1,22 @@
+export const NAV_LINK_CONFIG: {
+  path: string;
+  name: String;
+  id: number;
+}[] = [
+  {
+    path: "",
+    name: "Product",
+    id:1
+  },
+  {
+    path: "",
+    name: "Company",
+    id: 2,
+  },
+  {
+    path: "",
+    name: "Support",
+    id: 3,
+  },
+  
+];
